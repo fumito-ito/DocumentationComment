7ee90bd727aeb193f4fd7dd3dd6378af1a9573be
Fumito Ito <fumito.ito@nex.nikkei.com>
2024-06-20 11:00:51 +0900

    apple/swift-markdown is released for swift 5.10 !

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["DocumetationComment"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/fumito-ito/swift-markdown.git", from: "510.0.1"),
+        .package(url: "https://github.com/apple/swift-markdown.git", from: "0.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
